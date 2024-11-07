60aa611c992c18dbe006e0e8002488ecad1b70aa
Thyago Augusto <thyago.abfc@gmail.com>
2024-11-07 11:57:49 -0300

    criação do projeto

---
+++ 1_INTRO/index.js
@@ -0,0 +1,6 @@
+var x = 1;
+var y = 1;
+function soma(x, y) {
+    return x + y;
+}
+console.log(soma(x, y));
---
+++ 1_INTRO/index.ts
@@ -0,0 +1,8 @@
+const x = 1
+const y = 1
+
+function soma(x: number, y: number){
+    return x + y
+}
+
+console.log(soma(x, y))
